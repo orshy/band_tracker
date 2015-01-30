60d044c81c9ef8ef96ea46364f7a1236e82a2dcb
Philip Orchard <philiporchard@gmail.com>
2015-01-30 14:45:40 -0800

    Link band to blank output page

--- app.rb
+++ app.rb
@@ -4,5 +4,21 @@ Dir[File.dirname(__FILE__) + "/lib/*.rb"].each { |file| require file }
 
 
 get('/') do
+  @bands = Band.all()
+  @venues = Venue.all()
   erb(:index)
+end
+
+post('/') do
+  band_name = params.fetch("band_name")
+  Band.create({ :name => band_name })
+  venue_name = params.fetch("venue_name")
+  Venue.create({ :name => venue_name })
+  redirect('/')
+end
+
+get('/band/:id') do
+  @band = Band.find(params.fetch("id").to_i())
+  @venues = Venue.all()
+  erb(:band)
 end
